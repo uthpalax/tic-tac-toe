295596b4359e44b2a723a1d6b13aa5e9cec956eb
Uthpala Heenatigala <uthpala@passionatepeople.io>
2025-02-07 17:34:32 +0100

    completed the game need to update all winner combinations.

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,74 @@
 import { useState } from "react";
 import "./App.css";
 
-function Square({ clickedSqaures, position, setClickedSquares }) {
-  function handleClick() {
-    setClickedSquares([...clickedSqaures, position]);
-  }
-
-  function checkIfMarked() {
-    const clicked = clickedSqaures.find((square) => {
-      if (square.x === position.x && square.y === position.y) {
-        return true;
-      }
-      return false;
-    });
-    if (clicked) {
-      return "X";
-    }
-  }
-
-  return <span onClick={handleClick}> {checkIfMarked()}</span>;
+function Square({ position, handleClick }) {
+  return <div className="box" onClick={() => handleClick({ ...position, value: 'X' })}> {position.value} </div>;
 }
 
 function App() {
-  const [clickedSqaures, setClickedSquares] = useState([]);
-  console.log("clickedSqaures", clickedSqaures);
+  const [squares, setSquares] = useState([
+    { x: 0, y: 0, value: null },
+    { x: 0, y: 1, value: null },
+    { x: 0, y: 2, value: null },
+    { x: 1, y: 0, value: null },
+    { x: 1, y: 1, value: null },
+    { x: 1, y: 2, value: null },
+    { x: 2, y: 0, value: null },
+    { x: 2, y: 1, value: null },
+    { x: 2, y: 2, value: null },
+  ]);
+  const [player, setPlayer] = useState('X')
+  let winner = null
+
+  const winnerCombinations = [
+    [{ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }],
+    [{ x: 1, y: 0 }, { x: 1, y: 1 }, { x: 1, y: 2 }]
+  ]
+
+  function handleClick(position) {
+    const updatedSquares = squares.map((square) => {
+      if (square.x === position.x && square.y === position.y) {
+        return {
+          x: position.x,
+          y: position.y,
+          value: player
+        };
+      }
+      return square
+    });
+    setSquares(updatedSquares);
+    setPlayer(player === 'X' ? 'O' : 'X')
+  }
+
+  const xSquares = squares.filter(({ value }) => value === 'X')
+  const oSquares = squares.filter(({ value }) => value === 'O')
+
+  winnerCombinations.forEach((combination) => {
+    const areAllInXArray = combination.every(item1 =>
+      xSquares.some(item2 => item1.x === item2.x && item1.y === item2.y)
+    );
+    if (areAllInXArray) {
+      winner = 'X'
+    }
+    const areAllInOArray = combination.every(item1 =>
+      oSquares.some(item2 => item1.x === item2.x && item1.y === item2.y)
+    );
+    if (areAllInOArray) {
+      winner = 'O'
+    }
+  })
 
   return (
     <>
-      <div className="row">
-        <Square
-          position={{ x: 0, y: 2 }}
-          setClickedSquares={setClickedSquares}
-          clickedSqaures={clickedSqaures}
-        />
-        <Square
-          position={{ x: 1, y: 2 }}
-          setClickedSquares={setClickedSquares}
-          clickedSqaures={clickedSqaures}
-        />
-        <Square
-          position={{ x: 2, y: 2 }}
-          setClickedSquares={setClickedSquares}
-          clickedSqaures={clickedSqaures}
-        />
-      </div>
-      <div className="row">
-        <Square
-          position={{ x: 0, y: 1 }}
-          setClickedSquares={setClickedSquares}
-          clickedSqaures={clickedSqaures}
-        />
-        <Square
-          position={{ x: 1, y: 1 }}
-          setClickedSquares={setClickedSquares}
-          clickedSqaures={clickedSqaures}
-        />
-        <Square
-          position={{ x: 2, y: 1 }}
-          setClickedSquares={setClickedSquares}
-          clickedSqaures={clickedSqaures}
-        />
-      </div>
-      <div className="row">
-        <Square
-          position={{ x: 0, y: 0 }}
-          setClickedSquares={setClickedSquares}
-          clickedSqaures={clickedSqaures}
-        />
-        <Square
-          position={{ x: 1, y: 0 }}
-          setClickedSquares={setClickedSquares}
-          clickedSqaures={clickedSqaures}
-        />
-        <Square
-          position={{ x: 2, y: 0 }}
-          setClickedSquares={setClickedSquares}
-          clickedSqaures={clickedSqaures}
-        />
+      <h3>Winner {winner ? winner : '- no winner'}</h3>
+      <div className="game-board">
+        {squares.map(square => (
+          <Square
+            key={`${square.x}-${square.y}`}
+            position={square}
+            handleClick={handleClick}
+          />
+        ))}
       </div>
     </>
   );
